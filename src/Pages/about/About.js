bb82675797cd0a83ed9ee11f6fc973bedbbda092
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-05-17 10:04:06 +0500

    media query yozildi ozroq

--- src/Pages/about/About.js
+++ src/Pages/about/About.js
@@ -2,11 +2,6 @@ function About() {
   return (
     <div className="container">
       <h1>About</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure saepe non
-        quo qui optio, unde velit quod? At, perferendis! Impedit voluptas quod
-        ipsum incidunt, at perspiciatis. Nulla laborum corporis commodi?
-      </p>
     </div>
   );
 }
